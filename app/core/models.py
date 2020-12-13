28dd01e8e3562d40c4e4efb6e27085d53e17b4de
deoneloff <deoneloff@gmail.com>
2020-12-13 07:54:10 +0100

    Configured Django custom user model

---
+++ app/core/models.py
@@ -0,0 +1,34 @@
+from django.db import models
+from django.contrib.auth.models import AbstractBaseUser, BaseUserManager, PermissionsMixin
+from django.conf import settings
+
+
+class UserManager(BaseUserManager):
+    
+    def create_user(self, email, password=None):
+        if not email:
+            raise ValueError('User must have an email address')
+        email = self.normalize_email(email)
+        user = self.model(email=email)
+        user.set_password(password)
+        user.save(using=self._db)
+        return user
+
+    def create_superuser(self, email, password):
+        user = self.create_user(email, password)
+        user.is_superuser = True
+        user.is_staff = True
+        user.save(using=self._db)
+        return user
+
+class User(AbstractBaseUser, PermissionsMixin):
+    email = models.EmailField(max_length=255, unique=True)
+    is_active = models.BooleanField(default=True)
+    is_staff = models.BooleanField(default=False)
+
+    objects = UserManager()
+
+    USERNAME_FIELD = 'email'
+
+    def __str__(self):
+        return self.email
---
+++ app/core/tests/test_models.py
@@ -0,0 +1,54 @@
+from django.test import TestCase
+from django.contrib.auth import get_user_model
+
+class ModelTests(TestCase):
+
+        # Arrange
+        # Act
+        # Assert
+
+    def test_create_user_with_email_successful(self):
+        '''Test creating an user with an email is successful'''
+        # Arrange
+        email = 'janbrandewijn@mydomain.com'
+        password = 'Password123'
+        # Act
+        user = get_user_model().objects.create_user(
+            email=email,
+            password=password,
+        )
+        # Assert
+        self.assertEqual(user.email, email)
+        self.assertTrue(user.check_password(password))
+
+    def test_create_user_with_email_normalized(self):
+        '''Test the user email is normalized'''
+        # Arrange
+        email = 'janbrandewijn@MYDOMAIN.COM'
+        password = 'Password123'
+        # Act
+        user = get_user_model().objects.create_user(
+            email=email,
+            password=password,
+        )
+        # Assert
+        self.assertEqual(user.email, email.lower())
+
+    def test_create_user_invalid_email(self):
+        '''Test creating user with no email raises error'''
+        with self.assertRaises(ValueError): # anything running inside with-statement must raise a ValueError, else test will fail
+            get_user_model().objects.create_user(None, 'pass123')
+
+    def test_create_superuser_successful(self):
+        '''Test creating a super user is successful'''
+        # Arrange
+        email = 'janbrandewijn@mydomain.com'
+        password = 'Password123'
+        # Act
+        user = get_user_model().objects.create_superuser(
+            email=email,
+            password=password,
+        )
+        # Assert
+        self.assertTrue(user.is_staff)
+        self.assertTrue(user.is_superuser)
